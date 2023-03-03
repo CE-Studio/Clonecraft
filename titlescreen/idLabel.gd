e68d47da9b82ad899ce73c707085cd92eee4437e
clarence112 <clarencepacelli@gmail.com>
2023-03-03 04:47:27 -0500

    add some stuff to title

--- titlescreen/idLabel.gd
+++ titlescreen/idLabel.gd
@@ -3,5 +3,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    var vinfo := Engine.get_version_info()
     text = "Clonecraft "+ SettingManager.VERSION + "
+Engine version " + vinfo["string"] + ", " + str(vinfo["year"]) + "
 GPLv3 CE Studio. Please distribute!"
