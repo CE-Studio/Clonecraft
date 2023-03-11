791bc8a6d104e931796ebf7e23c480f4361c7ce3
clarence112 <clarencepacelli@gmail.com>
2023-03-11 02:43:06 -0500

    credits work (once) (also not quite as pretty as I'd like yet)

---
+++ gui/devpanel.gd
@@ -0,0 +1,33 @@
+extends Panel
+
+
+var dat := {}
+var content:Array[Control] = []
+
+
+func populate(data:Dictionary) -> void:
+    dat = data
+    $TextureRect.texture = load(dat["author_logo"])
+    $Label.text = dat["author_name"]
+    content.append(TextureRect.new())
+    content[-1].expand_mode = TextureRect.EXPAND_KEEP_SIZE
+    content[-1].stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
+    content[-1].size_flags_horizontal = Control.SIZE_EXPAND_FILL
+    content[-1].texture = load(dat["author_banner"])
+    content.append(Label.new())
+    content[-1].autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+    content[-1].text = dat["author_name"]
+    content.append(Label.new())
+    content[-1].autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+    content[-1].text = dat["author_desc"]
+    for i in dat["support_links"]:
+        content.append(LinkButton.new())
+        content[-1].text = i
+        content[-1].uri = i
+
+
+func _on_button_pressed():
+    var op = load("res://gui/backingpanel.tscn").instantiate()
+    $"/root".add_child(op)
+    for i in content:
+        op.addItem(i.duplicate())
