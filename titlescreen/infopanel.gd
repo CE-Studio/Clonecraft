004c6b8c39a61870561a4303865722268aec0d23
clarence112 <clarencepacelli@gmail.com>
2023-07-14 23:07:36 -0400

    add some info

---
+++ titlescreen/infopanel.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var _vp := get_viewport()
+var pos := 150.0
+
+func _input(event: InputEvent) -> void:
+    if event is InputEventMouseMotion:
+        if (event.position.x < 650) && (event.position.y > (_vp.size.y - 150)):
+            pos = 0
+        else:
+            pos = 150
+
+
+func _process(delta: float) -> void:
+    position.y = lerpf(position.y, pos, 10 * delta)
