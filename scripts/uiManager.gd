78fbfbb8a8546f4799f0afc797cef94260804949
clarence112 <clarencepacelli@gmail.com>
2023-09-17 04:47:44 -0400

    no message

--- scripts/uiManager.gd
+++ scripts/uiManager.gd
@@ -1,11 +1,2 @@
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-    pass
+extends Object
+class_name UiManager
