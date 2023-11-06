9b025e78bfc5ee50521bbefcae53d1a3515f94a8
clarence112 <clarencepacelli@gmail.com>
2023-11-05 22:08:13 -0500

    no message

--- scripts/chatControl.gd
+++ scripts/chatControl.gd
@@ -1,8 +1,12 @@
 # TODO make this generally better
 extends VBoxContainer
 class_name Chat
+## Manages displaying the chat.
 
+## Very, very WIP.
 
+## A refernce to the Chat singleton.[br]
+## Static
 static var instance:Chat
 const _line := preload("res://scripts/helpers/chat_label.tscn")
 
@@ -11,6 +15,8 @@ func _ready() -> void:
     instance = self
 
 
+## Display a line of text in the chat.[br]
+## Static
 static func pushText(text:String) -> void:
     if is_instance_valid(instance):
         var nline:Label = _line.instantiate()
