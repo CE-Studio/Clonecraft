ad499077b4b90ecf6950837e400dda511ae15ea3
clarence112 <clarencepacelli@gmail.com>
2024-03-04 23:23:58 -0500

    Update inventory.gd

--- scripts/game/inventory.gd
+++ scripts/game/inventory.gd
@@ -3,3 +3,28 @@ class_name Inventory
 ## A container for easily storing a collection of [ItemManager.ItemStack]s.
 
 
+@export var space:int = 2624
+var container:Array[ItemManager.ItemStack]
+var consumption:int
+	set(_val):
+		pass
+
+
+func save() -> Dictionary:
+	pass
+
+
+func load(inp:Dictionary) -> bool:
+	pass
+
+
+func addItem(item:ItemManager.ItemStack) -> bool:
+	pass
+
+
+func extractItem(item:ItemManager.ItemStack) -> bool:
+	pass
+
+
+func containsItem(item:ItemManager.ItemStack) -> bool:
+	pass
