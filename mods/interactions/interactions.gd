6e635c6b4861a236349dcdd86f7f0e92dda503d3
clarence112 <clarencepacelli@gmail.com>
2024-08-15 04:12:51 -0400

    breaking (is icky and hacky and needs some cleanup)

--- mods/interactions/interactions.gd
+++ mods/interactions/interactions.gd
@@ -9,7 +9,10 @@ var breaking := false
 var _oldplace := false
 var _placestart := Vector3i.ZERO
 var _breakpos:Vector3i
+var _breakprogress:float = 0
+var breakPower:float = 0
 var _highlight:MeshInstance3D
+var _break:MeshInstance3D
 
 
 func updatePlace() -> void:
@@ -47,6 +50,8 @@ func updatePlace() -> void:
 func _process(_delta:float) -> void:
 	if !WorldControl.isPaused():
 		if placing and (player.lookingAt != null):
+			if _breakprogress > 0:
+				_breakprogress = 0
 			var targpos:Vector3i
 			if Input.is_action_pressed("game_sneak"):
 				targpos = player.lookingAt.position
@@ -54,6 +59,25 @@ func _process(_delta:float) -> void:
 				targpos = player.lookingAt.previous_position
 			_highlight.position = ((_placestart + targpos) / 2.0) + Vector3(0.5, 0.5, 0.5)
 			_highlight.scale = Vector3((_placestart - targpos).abs()) + Vector3(1.05, 1.05, 1.05)
+		elif breaking and (player.lookingAt != null):
+			_break.show()
+			_break.position = player.lookingAt.position
+			_break.position += Vector3(0.5, 0.5, 0.5)
+			if _breakpos != player.lookingAt.position:
+				_breakpos = player.lookingAt.position
+				_breakprogress = 0
+				_break.material_override.set_shader_parameter(&"progress", 0)
+			else:
+				_breakprogress += _delta * breakPower
+				var s := man.getBlock(_breakpos).breakStrength
+				_break.material_override.set_shader_parameter(&"progress", round(remap(_breakprogress, 0, s, 0, 9)))
+				if _breakprogress >= s:
+					man.setBlock(_breakpos, &"clonecraft:air", not(player.abilities["endlessInventory"]))
+					_breakprogress = 0
+					_break.hide()
+					_break.material_override.set_shader_parameter(&"progress", 0)
+		elif breaking:
+			pass
 
 
 func _ununhandled_input(event:InputEvent) -> void:
@@ -96,9 +120,11 @@ func _ununhandled_input(event:InputEvent) -> void:
 						return
 				if ii.isTool:
 					return
+			breakPower = 1.0
 			breaking = true
 		elif event.is_action_released("game_break"):
 			breaking = false
+			_break.hide()
 			var i := player.getSelectedItem()
 			if is_instance_valid(i):
 				var ii := i.getItem()
@@ -124,3 +150,6 @@ func registerPhase() -> void:
 	_highlight = load("res://mods/interactions/highlight.tscn").instantiate()
 	_highlight.hide()
 	player.get_parent().add_child.call_deferred(_highlight)
+	_break = load("res://mods/interactions/break.tscn").instantiate()
+	_break.hide()
+	player.get_parent().add_child.call_deferred(_break)
