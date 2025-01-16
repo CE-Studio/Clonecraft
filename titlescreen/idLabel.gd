9b761bc78644cc91d8ef9e2373dbb8d0bc297415
clarence112 <clarencepacelli@gmail.com>
2025-01-16 04:03:03 -0500

    missed a spot

--- titlescreen/idLabel.gd
+++ titlescreen/idLabel.gd
@@ -6,5 +6,5 @@ func _ready():
 	var vinfo := Engine.get_version_info()
 	text = "Clonecraft "+ SettingManager.VERSION + "
 Engine version " + vinfo["string"] + ", " + str("2025") + "
-(C) CE Studio 2025. GPLv3. Please distribute!"
+(C) CE Studio 2025. AGPLv3. Please distribute!"
 	print("[Engine info] [Commit hash] " + vinfo["hash"])
